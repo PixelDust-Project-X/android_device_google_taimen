d1e47147d7d0ed43d12d7e46684c25fd59d98cff
Eamon Powell <eamonpowell@outlook.com>
2020-04-23 18:18:25 +1000

    Remove duplicate surface flinger property overrides
    * We already unset these property overrides in Wahoo tree
    
    Change-Id: I54fc65746a93632f9076323667fa5b5309edd9c5

--- device.mk
+++ device.mk
@@ -138,11 +138,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
   ro.vibrator.hal.heavyclick.duration=12
 
 # Early phase offset for SurfaceFlinger (b/75985430)
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_phase_offset_ns=1500000
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_app_phase_offset_ns=500000
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_gl_phase_offset_ns=1500000
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    debug.sf.early_phase_offset_ns=1500000
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    debug.sf.early_app_phase_offset_ns=500000
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    debug.sf.early_gl_phase_offset_ns=1500000
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    debug.sf.early_gl_app_phase_offset_ns=15000000
